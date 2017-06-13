556842d8296e8d7673c547947778d5c26c6b2a52
nofarshitrit <nofar44555@gmail.com>
2017-06-13 11:01:59 +0300

    large amount of changes

--- src/main/java/org/servicebroker/routeservice/config/CatalogConfig.java
+++ src/main/java/org/servicebroker/routeservice/config/CatalogConfig.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.collect.Lists;
 import org.springframework.cloud.servicebroker.model.Catalog;
 import org.springframework.cloud.servicebroker.model.Plan;
 import org.springframework.cloud.servicebroker.model.ServiceDefinition;
@@ -16,37 +15,29 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CatalogConfig {
-	
+
 	@Bean
 	public Catalog catalog() {
 		return new Catalog(Collections.singletonList(
 				new ServiceDefinition(
 						"route-service-broker",
 						"route service",
-						"A simple route service",
+						"A simple route service broker implementation",
 						true,
 						false,
-						Lists.newArrayList(
+						Collections.singletonList(
 								new Plan("route-service-plan",
 										"default",
-										"All services are created equally.",
+										"default plan for route sercvice",
 										getPlanMetadata())),
 						null,
-						getServiceDefinitionMetadata(),
-						Lists.newArrayList(ServiceDefinitionRequires.SERVICE_REQUIRES_ROUTE_FORWARDING.toString()),
+						null,
+						Arrays.asList(ServiceDefinitionRequires.SERVICE_REQUIRES_ROUTE_FORWARDING.toString()),
 						null)));
 	}
-	
+
 /* Used by Pivotal CF console */
 
-	private Map<String, Object> getServiceDefinitionMetadata() {
-		Map<String, Object> sdMetadata = new HashMap<>();
-		sdMetadata.put("displayName", "RouteService");
-		sdMetadata.put("longDescription", "Route Service");
-		sdMetadata.put("providerDisplayName", "Predix");
-		return sdMetadata;
-	}
-	
 	private Map<String,Object> getPlanMetadata() {
 		Map<String,Object> planMetadata = new HashMap<>();
 		planMetadata.put("costs", getCosts());
@@ -55,13 +46,14 @@ public class CatalogConfig {
 
 	private List<Map<String,Object>> getCosts() {
 		Map<String,Object> costsMap = new HashMap<>();
-		
+
 		Map<String,Object> amount = new HashMap<>();
 		amount.put("usd", 0.0);
-	
+
 		costsMap.put("amount", amount);
 		costsMap.put("unit", "MONTHLY");
-		
+
 		return Collections.singletonList(costsMap);
 	}
+
 }
