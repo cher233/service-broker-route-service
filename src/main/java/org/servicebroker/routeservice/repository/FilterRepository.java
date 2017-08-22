1e2a68b55291349e1aae365b6045e8d792b81d30
cher <cheramar@gmail.com>
2017-08-22 13:28:57 +0300

    added v2 to db

--- src/main/java/org/servicebroker/routeservice/repository/FilterRepository.java
+++ src/main/java/org/servicebroker/routeservice/repository/FilterRepository.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FilterRepository extends JpaRepository<FilterEntity,Integer> {
+
 }
---
+++ src/main/resources/db/migration/V2__deleteFilters.sql
@@ -0,0 +1,2 @@
+DELETE from route_service.filter_Info where filter_info.filter_id = 3;
+DELETE from route_service.filter_Info where filter_info.filter_id = 4;
